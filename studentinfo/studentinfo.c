3bc28edec8fabba27af03f4f699c99711a264679
vidyasagar seshadri <vidyasagar196@gmail.com>
2021-01-04 18:35:08 +0530

    Update studentinfo.c

--- studentinfo/studentinfo.c
+++ studentinfo/studentinfo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
+	//Understand this structure declaration
+	//1.Why are you declaring the structure with local scope?
+	//2.Understand structure padding & avoid structure padding?
 	struct student
 	{
 		char name[50];
